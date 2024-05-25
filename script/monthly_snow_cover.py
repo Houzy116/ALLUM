638f70f2797296e52bde0cd7377fcd1302c0545a
Houzy116 <131630519+Houzy116@users.noreply.github.com>
2024-05-25 19:52:38 +0800

    Delete script directory

--- script/monthly_snow_cover.py
+++
@@ -1,6 +0,0 @@
-from tool import *
-warnings.filterwarnings("ignore")
-if __name__=='__main__':
-    for y in range(2011,2021):
-        for m in range(1,13):
-            _=monthly_snow_cover(y,m,True)
--- script/run_ConfusionMatrix.py
+++
@@ -1,8 +0,0 @@
-from tool import *
-if __name__=='__main__':
-
-    for y in [2018]:    
-        for m in range(6,13):
-            print(f'------------------ {y}/{m} ------------------')
-            for lat_10 in tqdm(range(90,-90,-10)):
-                get_10lat_confusionmatrix(y,m,lat_10)
